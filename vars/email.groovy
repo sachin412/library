fd428e5d9a286941bb3b413a959d22397241594a
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-11 12:00:05 +0530

    adding log in email function

--- vars/email.groovy
+++ vars/email.groovy
@@ -2,6 +2,10 @@
 
 def call(String buildStatus = 'STARTED'){
 
-  mail bcc: '', body: 'please check your jobs console output "${BUILD_LOG}"', cc: '', from: '', replyTo: '', subject: 'job status failed', to: 'sachin.pavar@volansys.com'
+emailext attachLog: true, body: '''Hello,
+
+your current build status is failed. please check the status here.  
+
+thanks''', compressLog: true, subject: 'build is failed. please check the log from link', to: 'sachin.pavar@volansys.com'
 
 }
