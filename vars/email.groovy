8386ff5e9d4dd8e61b7b7df59e878b37a708b1dd
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-11 16:23:03 +0530

    ADDING BUILD URL

--- vars/email.groovy
+++ vars/email.groovy
@@ -4,7 +4,7 @@ def call(String buildStatus = 'STARTED'){
 
 emailext attachLog: true, body: '''Hello,
 
-your current build status is failed. please check the status here.  
+your current build status is failed. please check the status here ${BUILD_URL}.  
 
 thanks''', compressLog: true, subject: 'build is failed. please check the log', to: 'sachin.pavar@volansys.com'
 
