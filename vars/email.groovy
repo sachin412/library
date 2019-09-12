95eb7856c60811c6e6d8cddded65597cd4a28545
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-12 11:03:26 +0530

    Update email.groovy

--- vars/email.groovy
+++ vars/email.groovy
@@ -4,7 +4,7 @@ def call(String buildStatus = 'STARTED'){
 
 emailext attachLog: true, body: '''Hello,
 
-your current build status is failed. please check the status here ${BUILD_URL}.  
+your current build status is failed. please check the log here ${BUILD_URL}.  
 
 thanks''', compressLog: true, subject: 'build is failed. please check the log', to: 'sachin.pavar@volansys.com'
 
