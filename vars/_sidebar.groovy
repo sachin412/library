03f67b099fef6679f74ae4b27b14cd0f76c3f45b
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-12 18:35:30 +0530

    Update _sidebar.groovy

--- vars/_sidebar.groovy
+++ vars/_sidebar.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String buildStatus = 'STARTED'){
+def call(){
  
  addBadge icon: 'folder.gif', id: '', link: 'https://github.com/sachin412/newnode.git', text: ''
  addShortText background: 'yellow', borderColor: '', color: '', link: '', text: 'sachin412'
