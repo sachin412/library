c09c45147d51e7b00a5e08f1e4954d9c66669e3b
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-16 17:38:39 +0530

    creating jenkinsfile with docker env.

---
+++ vars/_dockerjenkins.groovy
@@ -0,0 +1,50 @@
+#!/usr/bin/env groovy
+
+def call(){
+ 
+  pipeline {
+    agent {
+        docker { image 'node'
+               args '-u 0' }
+    }
+    options { buildDiscarder(logRotator(numToKeepStr: '3')); timestamps() }    
+    stages {       
+        stage ('pre-build') {           
+            steps {
+                     sh 'npm install'     
+            }
+        }
+        stage('eslint') {
+            steps {                    
+                    _eslintcommand()                            
+            }
+        }
+     stage ('test') {
+            steps {
+                    _testrun()         
+            }
+        }
+      stage ('sonarqube') {
+            steps {
+                     _sonarqube()                    
+            }
+        }    
+    }   
+
+post {
+   failure {
+      email()
+      }
+  success {
+      _cobertura()
+      
+      }
+   always {       
+       _sidebar()  
+       _eslint()
+       _sonar()
+   } 
+  }
+ }
+
+}
