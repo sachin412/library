bdcfcdcd2d2fc765f48a3b8232df1e728bda22bd
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-19 16:43:11 +0530

    adding user

--- vars/_sidebar.groovy
+++ vars/_sidebar.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
 def call(){
- 
+ user='git show -s --pretty=%an'
  addBadge icon: 'folder.gif', id: '', link: 'https://github.com/sachin412/newnode.git', text: ''
- addShortText background: 'yellow', borderColor: '', color: '', link: '', text: 'sachin412'
+ addShortText background: 'yellow', borderColor: '', color: '', link: '', text: '$user'
  
 }
