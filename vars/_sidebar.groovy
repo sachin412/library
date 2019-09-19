a37cee508e5a5354af3181cc79e21ad60cad9394
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-19 17:14:49 +0530

    Update _sidebar.groovy

--- vars/_sidebar.groovy
+++ vars/_sidebar.groovy
@@ -4,5 +4,5 @@ def call(){
  //name='sh(script: '{git show -s --pretty=%an}', returnStdout: true)'
  addBadge icon: 'folder.gif', id: '', link: 'https://github.com/sachin412/newnode.git', text: ''
  // addShortText background: 'yellow', borderColor: '', color: '', link: '', text: '$name'
- sh(script: 'git show -s --pretty=%an', returnStdout: true) 
+// sh(script: 'git show -s --pretty=%an', returnStdout: true) 
 }
