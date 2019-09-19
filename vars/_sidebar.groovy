9ad064a9ce624c567c1c0ed74b778d6c50387ae4
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-19 18:15:53 +0530

    adding author name

--- vars/_sidebar.groovy
+++ vars/_sidebar.groovy
@@ -3,6 +3,6 @@
 def call(){ 
  //name='sh(script: '{git show -s --pretty=%an}', returnStdout: true)'
  addBadge icon: 'folder.gif', id: '', link: 'https://github.com/sachin412/newnode.git', text: ''
- // addShortText background: 'yellow', borderColor: '', color: '', link: '', text: '$name'
+ addShortText background: 'yellow', borderColor: '', color: '', link: '', text: '$GIT_AUTHOR_NAME'
 // sh(script: 'git show -s --pretty=%an', returnStdout: true) 
 }
