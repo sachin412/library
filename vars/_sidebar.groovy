23b3e6537b80deed3993e50a986ede8e724b05a0
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-19 17:03:50 +0530

    updating user name command

--- vars/_sidebar.groovy
+++ vars/_sidebar.groovy
@@ -3,6 +3,9 @@
 def call(){ 
  name='sh(script: '{git show -s --pretty=%an}', returnStdout: true)'
  addBadge icon: 'folder.gif', id: '', link: 'https://github.com/sachin412/newnode.git', text: ''
- addShortText background: 'yellow', borderColor: '', color: '', link: '', text: '$name'
+ // addShortText background: 'yellow', borderColor: '', color: '', link: '', text: '$name'
+ withEnv(['name=sh(script: \'git show -s --pretty=%an\', returnStdout: true)']) {
+ addShortText background: 'yellow', borderColor: '', color: '', link: '', text: "$name"
+}
  
 }
