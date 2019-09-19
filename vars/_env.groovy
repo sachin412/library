ca51b98f4b943c5ab2d7362f5fb2b93cafd6543d
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-19 19:54:21 +0530

    Update _env.groovy

--- vars/_env.groovy
+++ vars/_env.groovy
@@ -1,8 +1,7 @@
 #!/usr/bin/env groovy
 
-def call(){
-   sh 'cd /var/lib/jenkins/workspace/'
-   name=$(git show -s --pretty=%an)
+def call(){  
+
   name = sh(script: 'git show -s --pretty=%an' , returnStdout: true)
-   println name
+ echo $name
 }
