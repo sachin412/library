138e92c74979d841fd60b35d5fcb01be25189257
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-19 17:06:39 +0530

    adding git user command

--- vars/_sidebar.groovy
+++ vars/_sidebar.groovy
@@ -4,8 +4,5 @@ def call(){
  name='sh(script: '{git show -s --pretty=%an}', returnStdout: true)'
  addBadge icon: 'folder.gif', id: '', link: 'https://github.com/sachin412/newnode.git', text: ''
  // addShortText background: 'yellow', borderColor: '', color: '', link: '', text: '$name'
- withEnv(['name=sh(script: \'git show -s --pretty=%an\', returnStdout: true)']) {
- addShortText background: 'yellow', borderColor: '', color: '', link: '', text: "$name"
-}
- 
+ sh(script: 'git show -s --pretty=%an', returnStdout: true) 
 }
