d318301831aa6eb14c08313d697208174e2ab6af
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-19 18:03:34 +0530

    Update _env.groovy

--- vars/_env.groovy
+++ vars/_env.groovy
@@ -4,7 +4,7 @@ def call(){
      git 'https://github.com/sachin412/library.git'
     
     withEnv(['name={git show -s --pretty=%an}']) {
-   
+    sh 'cd /var/lib/jenkins/workspace/api-testing'
     sh 'eval "$name"'
   }
 }
