b4e1e3ac89230ddbf86e2ad9584ba2e823280d1d
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-20 11:56:54 +0530

    Update _sidebar.groovy

--- vars/_sidebar.groovy
+++ vars/_sidebar.groovy
@@ -5,5 +5,5 @@ def call(){
  addBadge icon: 'folder.gif', id: '', link: 'https://github.com/sachin412/newnode.git', text: ''
  // addShortText background: 'yellow', borderColor: '', color: '', link: '', text: 'sachin412'
  sh 'git log -1 | grep Author | awk "{print $2}" > name.txt'
- 
+ sh 'cat name.txt'
  }
