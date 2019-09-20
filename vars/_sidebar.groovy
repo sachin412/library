baf81beeb84d7a034eac2e37c6c828a92b0a8a75
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-20 11:37:48 +0530

    coorecting formatting errors

--- vars/_sidebar.groovy
+++ vars/_sidebar.groovy
@@ -5,6 +5,6 @@ def call(){
  addBadge icon: 'folder.gif', id: '', link: 'https://github.com/sachin412/newnode.git', text: ''
  addBadge icon: 'folder.gif', id: '', link: '', text: '${'git log -1 | grep Author | awk "{print $2}"'}'
 // addShortText background: 'yellow', borderColor: '', color: '', link: '', text: 'sachin412'
- sh 'git log -1 | grep Author | awk "{print $2}" > name.txt'
+ sh 'echo (git log -1 | grep Author | awk "{print $2}" > name.txt)'
  sh 'cat name.txt'
  }
