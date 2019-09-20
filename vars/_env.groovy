ef858025242b4cb1a2b5e83b158c0d6414de47b9
sachin412 <49905353+sachin412@users.noreply.github.com>
2019-09-20 11:18:07 +0530

    Update _env.groovy

--- vars/_env.groovy
+++ vars/_env.groovy
@@ -2,8 +2,7 @@
 
 def call(){  
 
-    sh ' git log -1 | grep Author | awk "{print $2}"'
+    sh 'name = git log -1 | grep Author | awk "{print $2}"'
     
-   sh 'echo "${git show -s --pretty=%an}"'
- 
+    
 }
